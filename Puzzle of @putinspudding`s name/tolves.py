6fa3807349189d11c73e70546d3714b910c3dc15
tolves <tov.sharing@gmail.com>
2018-06-20 13:46:27 +0800

    add exception and other solutions

--- Puzzle of @putinspudding`s name/tolves.py
+++ Puzzle of @putinspudding`s name/tolves.py
@@ -1,29 +1,49 @@
 # -*- coding: utf-8 -*-
+import sys
 class Puzzle:
     def __init__(self):
         
         return None
 
-    def main(self, dic_path, out_path):
-        
-        f = open(dic_path, encoding='utf-8')
-        
-        surname = f.readline().split(" ")
-        
-        #for line in f:
-        #    line=line.strip('\n').strip('\t')
-        #    surname.append(line)
-        #surname = surname[0].split(" ")
+    def run(self, dic_path, out_path):
 
-        out = '\n'.join(str(x)+'日天' for x in surname)
+        try:
 
-        #out = list(map(lambda x: x+'日天', surname))
+            f = open(dic_path, encoding='utf-8')
 
-        o = open(out_path, 'w', encoding='utf-8')
+        except:
 
-        o.write(str(out))
+            print(sys.exc_info()[1])
+
+        else:
+
+            surname = f.readline().split(" ")
+
+            # the other solution: split str into list
+            #for line in f:
+            #    line=line.strip('\n').strip('\t')
+            #    surname.append(line)
+            #surname = surname[0].split(" ")
+            
+            f.close
+            
+
+            out = '\n'.join(str(x) + '日天' for x in surname)
+
+            # the second solution to add 日天
+            #out = list(map(lambda x: x+'日天', surname))
+
+            # the third soulution to add 日天
+            #out = []
+            #for x in surname:
+            #    out.append(x + '日天')
+                
+            o = open(out_path, 'w', encoding='utf-8')
+
+            o.write(str(out))
+
+            o.close
         
-        #print(out)
 x = Puzzle()
-x.main('Hundred_Family_Surnames.txt','tolves_out.txt')
+x.run('Hundred_Family_Surnames.txt','tolves_out.txt')
 
