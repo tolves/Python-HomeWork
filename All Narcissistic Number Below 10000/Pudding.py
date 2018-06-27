edc0bf570a3ec503e9ec5c3dbcfbe59b1073e970
Putinspudding <35836135+Putinspudding@users.noreply.github.com>
2018-06-27 19:13:19 +0800

    0.01s less,one less quantity

--- All Narcissistic Number Below 10000/Pudding.py
+++ All Narcissistic Number Below 10000/Pudding.py
@@ -1,25 +1,43 @@
-def PowSum(power,numbers,digit):
+import time
+
+def PowSum(numbers):
     Sum=0
-    New_list=iteration(numbers,digit)
-    for n in range(0,digit):
-        Sum=New_list[n]**power+Sum
+    New_list=iteration(numbers)
+    x=digits(numbers)
+    
+    for n in range(0,x):
+        Sum=int(New_list[n])**x+Sum
     return Sum
 
-def flower(Power,Numbers,Digit):
-    if PowSum(Power,Numbers,Digit)==Numbers:
+def flower(Numbers):
+    if PowSum(Numbers)==Numbers:
         print(Numbers,'\n')
 
-def iteration(numb,times=4):
-    Origin_list=[]
-    for i in range(0,times):
-        Origin_list.append((numb//(10**(times-1-i)))%10)
-    return Origin_list
+def iteration(numb):
+    # Origin_list=[]
+    # for i in range(0,times):
+    #     Origin_list.append((numb//(10**(times-1-i)))%10)
+    # return Origin_list
+
+    return list(str(numb))
 
 def digits(nu):
     for i in range(0,100):
         if nu//(10**i)==0:
             return i
+    # return 4
+
+start = time.clock()
+
+
 
 for num in range(0,10000):
-    list1=iteration(num,digits(num))
-    flower(digits(num),num,digits(num))
+	# x = digits(num)
+	# list1=iteration(num,int(x))
+    # list1=iteration(num,digits(num))
+	flower(num)
+    # flower(digits(num),num,digits(num))
+
+elapsed = (time.clock() - start)
+
+print("Time used:",elapsed)
